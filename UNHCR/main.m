c28ce6dfcbd1081d6db7cfbb830e398748b1ed74
Sean Conrad <toblerpwn@gmail.com>
2013-09-30 09:36:52 -0700

    Initial Commit

---
+++ UNHCR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UNHCR
+//
+//  Created by Sean Conrad on 9/30/13.
+//  Copyright (c) 2013 Sean Conrad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HCRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HCRAppDelegate class]));
+    }
+}
---
+++ UNHCRTests/UNHCRTests.m
@@ -0,0 +1,34 @@
+//
+//  UNHCRTests.m
+//  UNHCRTests
+//
+//  Created by Sean Conrad on 9/30/13.
+//  Copyright (c) 2013 Sean Conrad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UNHCRTests : XCTestCase
+
+@end
+
+@implementation UNHCRTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
